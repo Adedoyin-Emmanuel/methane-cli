684110c6cbca59bd72ec7c8bf0866e2809a015ae
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-26 20:35:46 +0100

    Added user configuration files

--- index.js
+++ index.js
@@ -18,61 +18,9 @@ figlet.text(
   },
   function (err, data) {
     if (err) {
-      console.log("Figlet error:", err);
+      console.log("Figlet error:", colors.red(err));
       return;
     }
-        console.log(colors.rainbow(data));
-
-    program
-      .version("1.0.0")
-      .description("A React component generator")
-      .command("generate <name>")
-      .alias("g")
-      .description("Generate a new React component")
-      .action(async (name) => {
-        const componentName = name.charAt(0).toUpperCase() + name.slice(1);
-        const componentPath = path.join(process.cwd(), componentName);
-        const cssFilePath = path.join(componentPath, "styles.css");
-        const componentFilePath = path.join(
-          componentPath,
-          `${componentName}.jsx`
-        );
-
-        try {
-          // Create the component directory
-          await fs.mkdir(componentPath);
-
-          // Create the styles file
-          await fs.writeFile(cssFilePath, "");
-
-          // Create the component file
-          await fs.writeFile(
-            componentFilePath,
-            `import React from 'react';
-import './styles.css';
-
-const ${componentName} = () => {
-  return (
-    <div>
-      <h1>${componentName}</h1>
-    </div>
-  );
-};
-
-export default ${componentName};
-`
-          );
-
-          console.log(
-            colors.green(
-              `Component '${componentName}' generated successfully at '${componentPath}'`
-            )
-          );
-        } catch (error) {
-          console.error(colors.red(error));
-        }
-      });
-
-    program.parse(process.argv);
+    console.log(colors.rainbow(data));
   }
 );
