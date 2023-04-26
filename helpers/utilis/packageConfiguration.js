5c8db5e8c892c91a2b88706b105922ee0972fe52
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-27 00:22:36 +0100

    Added the packageConfiguration file

---
+++ helpers/utilis/packageConfiguration.js
@@ -0,0 +1,83 @@
+const userReadConfig = require("./readUserConfig");
+const userWriteConfig = require("./writeUserConfig");
+const {program} = require("commander");
+const colors = require("colors");
+
+const configurePackage = () => {
+  program
+    .command("config")
+    .description("Set default configurations")
+    .option(
+      "-t, --template " + colors.green("<template>"),
+      "Set the default template (jsx or tsx)"
+    )
+    .option(
+      "-c, --component " + colors.green("<component>"),
+      "Set the default component type (arrow or functional)"
+    )
+    .option(
+      "-p, --page " + colors.green("<page>"),
+      "Set the default page type (arrow or functional)"
+    )
+    .option(
+      "-r --register" + colors.green("<register>"),
+      "Set the default registration of pages in the App.jsx or App.tsx file (true or false)"
+    )
+    .action((options) => {
+      const config = userReadConfig.readConfig();
+      if (options.template) {
+        if (options.template != "jsx" && options.template != "tsx") {
+          console.log(options.template);
+          console.log(
+            colors.red(
+              "Invalid template format, format must be either 'jsx' or 'tsx'"
+            )
+          );
+          return;
+        }
+        config.template = options.template;
+        userWriteConfig.writeConfig(config);
+        console.log(colors.cyan("Default template is now " + config.template));
+      }
+      if (options.component) {
+        if (options.component != "arrow" && options.component != "functional") {
+          console.log(
+            colors.red(
+              "Invalid component format, format must be either 'arrow' or 'functional'"
+            )
+          );
+          return;
+        }
+        config.component = options.component;
+        userWriteConfig.writeConfig(config);
+        console.log(
+          colors.cyan(
+            "Default component is now " +
+              config.component +
+              " function template"
+          )
+        );
+      }
+      if (options.page) {
+        if (options.page != "arrow" && options.page != "functional") {
+          console.log(
+            colors.red(
+              "Invalid page format, format must be either 'arrow' or 'functional'"
+            )
+          );
+          return;
+        }
+        config.page = options.page;
+        userWriteConfig.writeConfig(config);
+        console.log(
+          colors.cyan(
+            "Default page is now " + config.page + " function template"
+          )
+        );
+      }
+    });
+};
+
+module.exports = {
+  configurePackage,
+};
--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 #!/usr/bin/env node
-
-const fs = require("fs-extra");
 const colors = require("colors");
 const figlet = require("figlet");
-const path = require("path");
 const { program } = require("commander");
-const userReadConfig = require("./helpers/utilis/readUserConfig");
-const userWriteConfig = require("./helpers/utilis/writeUserConfig");
-const gradient = require("gradient-string");
-const { option } = require("yargs");
-
+const packageConfig = require("./helpers/utilis/packageConfiguration");
 // Configure Figlet for the package name
 figlet.text(
   "React-Gen",
@@ -27,91 +20,33 @@ figlet.text(
     }
 
     console.log(colors.rainbow(data));
-
+    
+    //package description
     program
       .version("1.0.0")
       .description(
         colors.cyan("A CLI tool that creates React Components & Pages")
-      );
-    program
-      .command("config")
-      .description("Set default configurations")
-      .option(
-        "-t, --template " + colors.green("<template>"),
-        "Set the default template (jsx or tsx)"
-      )
-      .option(
-        "-c, --component " + colors.green("<component>"),
-        "Set the default component type (arrow or functional)"
-      )
+    );
+    program 
+      .command("generate")
+      .alias("g")
       .option(
         "-p, --page " + colors.green("<page>"),
-        "Set the default page type (arrow or functional)"
-      ).option(
-        "-r --register" + colors.green("<register>"),
-        "Set the default registration of pages in the App.jsx or App.tsx file (true or false)" 
-      )
-      .action((options) => {
-        const config = userReadConfig.readConfig();
-        if (options.template) {
-          if (options.template != "jsx" && options.template != "tsx") {
-            console.log(options.template);
-            console.log(
-              colors.red(
-                "Invalid template format, format must be either 'jsx' or 'tsx'"
-              )
-            );
-            return;
-          }
-          config.template = options.template;
-          userWriteConfig.writeConfig(config);
-          console.log(
-            colors.cyan("Default template is now " + config.template)
-          );
-        }
-        if (options.component) {
-          if (
-            options.component != "arrow" &&
-            options.component != "functional"
-          ) {
-            console.log(
-              colors.red(
-                "Invalid component format, format must be either 'arrow' or 'functional'"
-              )
-            );
-            return;
-          }
-          config.component = options.component;
-          userWriteConfig.writeConfig(config);
-          console.log(
-            colors.cyan(
-              "Default component is now " + config.component + " function template"
-            )
-          );
-        }
-        if (options.page) {
-          if (
-            options.page != "arrow" &&
-            options.page != "functional"
-          ) {
-            console.log(
-              colors.red(
-                "Invalid page format, format must be either 'arrow' or 'functional'"
-              )
-            );
-            return;
-          }
-          config.page = options.page;
-          userWriteConfig.writeConfig(config);
-          console.log(
-            colors.cyan(
-              "Default page is now " +
-                config.page +
-                " function template"
-            )
-          );
-        }
-      });
+        "Generates a React page file"
+       )
+      .option(
+        "-c, --component "+ colors.green("<component>"),
+        "Generates a React component file"
+       )
+      .description("Generate a new React Component or Page!")
+      .action(async (name)=>{
+          console.log(name);
+          
+      })
+      
+    //package configurations.
+    packageConfig.configurePackage();
+    
     program.parse(process.argv);
   }
 );
