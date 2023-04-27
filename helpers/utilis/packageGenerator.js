a197058a719aed66066498c9e2b2863ce41139fe
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-27 17:19:40 +0100

    Added the generatePage module

--- helpers/utilis/packageGenerator.js
+++ helpers/utilis/packageGenerator.js
@@ -1,6 +1,7 @@
 const colors = require("colors");
 const { program } = require("commander");
 const componentGen = require("./../generators/generateComponent");
+const pageGen = require("./../generators/generatePage")
 const packageGenerator = () => {
   program
     .command("generate")
@@ -45,6 +46,7 @@ const packageGenerator = () => {
         const pageName = `${page.charAt(0).toUpperCase()}${page.slice(1)}`;
         //serviceworker is here too
         componentGen.generateComponent(componentName);
+        pageGen.generatePage(pageName);
         
       } else if (
         component !== undefined &&
@@ -58,11 +60,13 @@ const packageGenerator = () => {
         componentGen.generateComponent(componentName);
         
       } else if (
-        name !== undefined &&
+        page !== undefined &&
         component === undefined &&
         serviceWorker === undefined
       ) {
         const pageName = `${page.charAt(0).toUpperCase()}${page.slice(1)}`;
+        pageGen.generatePage(pageName);
+        
       } else if (
         serviceWorker !== undefined &&
         component === undefined &&
@@ -75,6 +79,8 @@ const packageGenerator = () => {
         component === undefined
       ) {
         const pageName = `${page.charAt(0).toUpperCase()}${page.slice(1)}`;
+        pageGen.generatePage(pageName);
+        
         //serviceworker is here too
       } else if (
         serviceWorker !== undefined &&
@@ -84,6 +90,8 @@ const packageGenerator = () => {
         const componentName = `${component
           .charAt(0)
           .toUpperCase()}${component.slice(1)}`;
+         componentGen.generateComponent(componentName);
+          
         //service worker is here too
       } else if (
         page !== undefined &&
@@ -94,6 +102,9 @@ const packageGenerator = () => {
           .charAt(0)
           .toUpperCase()}${component.slice(1)}`;
         const pageName = `${page.charAt(0).toUpperCase()}${page.slice(1)}`;
+        pageGen.generatePage(pageName);
+        componentGen.generateComponent(componentName);
+        
       }
     });
 };
