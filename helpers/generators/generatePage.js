63ee629e2e1ccdd21eb09e77f446357535f9fa50
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-27 18:32:40 +0100

    Updated the generatePage to only add the pageImport if configured

--- helpers/generators/generatePage.js
+++ helpers/generators/generatePage.js
@@ -64,7 +64,8 @@ const generatePage = async (name) => {
     );
     
     /*register the page in the App.jsx or App.tsx file*/
-    pageRegisterer.addPageImport(rootDir, name, pageDir);
+    (readUserConfig.readConfig().register === "true") ? pageRegisterer.addPageImport(rootDir, name, pageDir) : null;
+    
   } catch (error) {
     console.log(colors.bold(colors.red(error)));
   }
