d11e47655e212f2b2a2027fceb0316738f534ae1
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-04-28 07:32:23 +0100

    Updated package.json

--- helpers/utilis/packageDescription.js
+++ helpers/utilis/packageDescription.js
@@ -6,7 +6,7 @@ const packageDescription = () => {
     .version("1.0.0")
     .description(
       colors.cyan(
-        "A CLI tool for efficient creation of React components, pages and Service Worker files."
+        "Methane-Cli is a tool for efficient creation of React components, pages and Service Worker files."
       )
     )
 };
