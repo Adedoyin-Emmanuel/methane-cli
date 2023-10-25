38fff8cca9ca7c42306a6b0edaf5d143e7ee3b09
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-10-25 15:49:16 +0100

    Feat: Added nextJs dymanic page template

---
+++ helpers/generators/templates/nextJsDynamicPage.ts
@@ -0,0 +1,175 @@
+const generateNextJsDynamicPageTsxArrowTemplate = (
+  dynamicPageId: string,
+  importStyleSheet: string,
+  componentName: string,
+  styleSheetType: string,
+  componentType: string
+) => {
+  let template: string;
+  let componentTypeTemplate = componentType === "client" && "use client";
+  let addStyleSheetImport =
+    importStyleSheet === "true" &&
+    `import "./${componentName}Style.${styleSheetType}";`;
+  let addRouterImport =
+    componentType === "client" &&
+    'import { useRouter } from "next/navigation";';
+
+  let dynamicPageIdDisplay = `\${params.${dynamicPageId}}`;
+
+  template = `
+${componentTypeTemplate}
+
+import React from "react";
+${addStyleSheetImport}
+${addRouterImport}
+
+const ${componentName} = ({ params }: { params: { ${dynamicPageId}: string } }) => {
+
+    return (
+        <div>
+            <h1>${componentName} works!</h1>
+            <p>${dynamicPageIdDisplay}</p>
+        </div>
+    );  
+}
+
+export default ${componentName};
+    `;
+  return template;
+};
+
+const generateNextJsDynamicPageTsxFunctionTemplate = (
+  dynamicPageId: string,
+  importStyleSheet: string,
+  componentName: string,
+  styleSheetType: string,
+  componentType: string
+) => {
+  let template: string;
+  let componentTypeTemplate = componentType === "client" && "use client";
+  let addStyleSheetImport =
+    importStyleSheet === "true" &&
+    `import "./${componentName}Style.${styleSheetType}";`;
+  let addRouterImport =
+    componentType === "client" &&
+    'import { useRouter } from "next/navigation";';
+
+  let dynamicPageIdDisplay = `\${params.${dynamicPageId}}`;
+
+  template = `
+${componentTypeTemplate}
+
+import React from "react";
+${addStyleSheetImport}
+${addRouterImport}
+
+function ${componentName} ({ params }: { params: { ${dynamicPageId}: string } }){
+
+    return (
+        <div>
+            <h1>${componentName} works!</h1>
+            <p>${dynamicPageIdDisplay}</p>
+        </div>
+    );  
+}
+
+export default ${componentName};
+    `;
+
+  return template;
+};
+
+const generateNextJsDynamicPageJsxArrowTemplate = (
+  dynamicPageId: string,
+  importStyleSheet: string,
+  componentName: string,
+  styleSheetType: string,
+  componentType: string
+) => {
+  let template: string;
+  let componentTypeTemplate = componentType === "client" ? "use client" : "";
+  let addStyleSheetImport =
+    importStyleSheet === "true"
+      ? `import "./${componentName}Style.${styleSheetType}";`
+      : "";
+  let addRouterImport =
+    componentType === "client"
+      ? 'import { useRouter } from "next/navigation";'
+      : "";
+
+  let dynamicPageIdDisplay =
+    dynamicPageId + " is ${params." + dynamicPageId + "}";
+
+  template = `
+${componentTypeTemplate}
+
+import React from "react";
+${addStyleSheetImport}
+${addRouterImport}
+
+const ${componentName} = ({ params }) => {
+
+    return (
+        <div>
+            <h1>${componentName} works!</h1>
+            <p>${dynamicPageIdDisplay}</p>
+        </div>
+    );  
+}
+
+export default ${componentName};
+  `;
+
+  return template;
+};
+
+const generateNextJsDynamicPageJsxFunctionTemplate = (
+  dynamicPageId: string,
+  importStyleSheet: string,
+  componentName: string,
+  styleSheetType: string,
+  componentType: string
+) => {
+  let template: string;
+  let componentTypeTemplate = componentType === "client" ? "use client" : "";
+  let addStyleSheetImport =
+    importStyleSheet === "true"
+      ? `import "./${componentName}Style.${styleSheetType}";`
+      : "";
+  let addRouterImport =
+    componentType === "client"
+      ? 'import { useRouter } from "next/navigation";'
+      : "";
+
+  let dynamicPageIdDisplay =
+    dynamicPageId + " is ${params." + dynamicPageId + "}";
+
+  template = `
+${componentTypeTemplate}
+
+import React from "react";
+${addStyleSheetImport}
+${addRouterImport}
+
+function ${componentName} ({ params }) {
+
+    return (
+        <div>
+            <h1>${componentName} works!</h1>
+            <p>${dynamicPageIdDisplay}</p>
+        </div>
+    );  
+}
+
+export default ${componentName};
+  `;
+
+  return template;
+};
+
+export {
+  generateNextJsDynamicPageJsxArrowTemplate,
+  generateNextJsDynamicPageJsxFunctionTemplate,
+  generateNextJsDynamicPageTsxArrowTemplate,
+  generateNextJsDynamicPageTsxFunctionTemplate,
+};
