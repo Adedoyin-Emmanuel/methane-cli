b65b3c4ad0bbde8b2d24ff8666ca8fa6bfb9be5c
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-06-28 08:43:10 +0100

    Feat: Changed package version number

--- helpers/utilis/packageDescription.ts
+++ helpers/utilis/packageDescription.ts
@@ -3,7 +3,7 @@ import colors from "colors";
 import { program } from "commander";
 const packageDescription = () => {
   program
-    .version("1.4.5")
+    .version("1.4.7")
     .description(
       colors.cyan(
         "Methane-Cli is a tool for efficient creation of React or NextJs components, pages and Service Worker files."
